1263e7b794494f3d63529da925b9b304ace141a0
Biswajit1244 <55921784+Biswajit1244@users.noreply.github.com>
2019-11-16 23:40:35 +0530

    Save object in Database

---
+++ HBProj-1_SaveObj/src/com/biss/entity/EmployeeEntity.java
@@ -0,0 +1,40 @@
+package com.biss.entity;
+
+public class EmployeeEntity {
+	private int empId;
+	private String eName;
+	private double sal;
+	private String job;
+	
+	//setters and getters method
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmployeeEntity [empId=" + empId + ", eName=" + eName + ", sal=" + sal + ", job=" + job + "]";
+	}
+}
